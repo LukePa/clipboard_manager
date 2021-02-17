c94c2d14cca85b3998be23b987f3799a85340750
lukep <lukep@DESKTOP-OP74DBP>
2021-02-17 22:04:00 +0000

    Turned clip representation from string to its own object

--- clipboard_manager.py
+++ clipboard_manager.py
@@ -8,17 +8,15 @@ class Clip_management_page(tk.Frame):
         super().__init__(controller)
         self.setup_initial_variables()
         self.controller = controller
-        self.model = clip_board_manager_model(self, clips_file_name)
+        self.model = Clip_board_manager_model(self, clips_file_name)
         self.create_create_clip_button()
         self.create_wipe_clips_button()
         self.create_main_menu_button()
-
 
     def setup_initial_variables(self):
         self.initial_y = 2
         self.buttons_list = []
         self.current_y = self.initial_y
-
 
     def create_create_clip_button(self):
         new_clip_button = tk.Button(self)
@@ -29,13 +27,11 @@ class Clip_management_page(tk.Frame):
         clips_label["text"] = "Created clips:"
         clips_label.grid(column = 0, row = 1, columnspan = 3)
 
-
     def create_wipe_clips_button(self):
         wipe_clips_button = tk.Button(self)
         wipe_clips_button["text"] = "<Click here to wipe clips>"
         wipe_clips_button["command"] = self.model.wipe_clips
         wipe_clips_button.grid(column = 1, row = 0)
-
 
     def create_main_menu_button(self):
         main_menu_button = tk.Button(self)
@@ -43,20 +39,18 @@ class Clip_management_page(tk.Frame):
         main_menu_button["command"] = self.controller.load_main_menu
         main_menu_button.grid(column=2, row = 0)
 
-
-    def create_new_clip_button(self, message):
+    def create_new_clip_button(self, clip):
         clip_button = tk.Button(self)
-        clip_button["text"] = message
-        clip_button["command"] = lambda: self.model.write_to_clipboard(message)
+        clip_button["text"] = clip.get_title()
+        clip_button["command"] = lambda: self.model.write_to_clipboard(clip.get_message())
         clip_button.grid(column = 0, row = self.current_y, columnspan = 3)
         delete_clip_button = tk.Button(self)
         delete_clip_button["text"] = "x"
-        delete_clip_button["command"] = lambda: self.model.remove_clip(message)
+        delete_clip_button["command"] = lambda: self.model.remove_clip(clip)
         delete_clip_button.grid(column = 3, row = self.current_y)
         self.current_y += 1
         self.buttons_list.append(clip_button)
         self.buttons_list.append(delete_clip_button)
-        
 
     def destroy_current_buttons(self):
         self.current_y = self.initial_y
@@ -65,25 +59,20 @@ class Clip_management_page(tk.Frame):
         self.buttons_list = []
         
 
-    
 
-
-
-class clip_board_manager_model(object):
+class Clip_board_manager_model(object):
 
     def __init__(self, clip_manager_page, clips_file_name):
         self.ui = clip_manager_page
         self.setup_initial_variables()
         self.load_new_clip_file(clips_file_name)
         
-
     def setup_initial_variables(self):
         self.clips_list = []
         try:
             os.mkdir('./clip_files')
         except:
             1+1
-
 
     def load_new_clip_file(self, filename):
         self.ui.destroy_current_buttons()
@@ -95,33 +84,42 @@ class clip_board_manager_model(object):
         except:
             self.clips_list = []
             self.update_saved_clips()
+        self.convert_strings_to_clips()
         self.populate_clip_buttons()
 
+    def convert_strings_to_clips(self):
+        for i in range(len(self.clips_list)):
+            clip = self.clips_list[i]
+            if type(clip) == str:
+                clipObj = Clip(clip)
+                self.clips_list[i] = clipObj
 
     def update_saved_clips(self):
         f = open(self.save_file_path, "wb")
         pickle.dump(self.clips_list, f)
         f.close()
 
-
     def wipe_clips(self):
         os.remove(self.save_file_path)
         self.ui.controller.load_main_menu()
 
-
-    def remove_clip(self, clip_str):
+    def remove_clip(self, clip):
         self.ui.destroy_current_buttons()
-        self.clips_list.remove(clip_str)
+        self.clips_list.remove(clip)
         self.update_saved_clips()
         self.populate_clip_buttons()
 
-
     def write_to_clipboard(self, input_string):
         if type(input_string) != str:
-            raise TypeError("Input String must be a string")
+            raise TypeError("Input String must be a clip")
         self.ui.clipboard_clear()
         self.ui.clipboard_append(input_string)
 
+    def get_all_clip_messages(self):
+        message_list = []
+        for clip in self.clips_list:
+            message_list.append(clip.get_message())
+        return message_list
 
     def create_new_clip_button_from_clipboard(self):
         """Verifies clipboard is valid before creating a new clip button"""
@@ -129,18 +127,42 @@ class clip_board_manager_model(object):
             message = self.ui.clipboard_get()
             if message == "":
                 return None
-            elif message in self.clips_list:
+            elif message in self.get_all_clip_messages():
                 return None
-            self.ui.create_new_clip_button(message)
-            self.clips_list.append(message)
+            clip = Clip(message)
+            self.ui.create_new_clip_button(clip)
+            self.clips_list.append(clip)
             self.update_saved_clips()
-        except:
-            1+1
-
+        except Exception as error:
+            print(error)
 
     def populate_clip_buttons(self):
         for clip in self.clips_list:
             self.ui.create_new_clip_button(clip)
+
+
+
+class Clip(object):
+
+    def __init__(self, message):
+        self._message = message
+        self._title = ""
+
+    def get_title(self):
+        if self._title:
+            return self._title
+        else:
+            return self._message
+
+    def set_title(self, title):
+        if type(title) != str:
+            raise TypeError
+        else:
+            self._title = title
+
+    def get_message(self):
+        return self._message
+        
         
     
   
